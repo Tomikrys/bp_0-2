6d615a772d9c0ac5c39f2e8482d656169a50162f
Tomáš Ryšavý <tomikrys@seznam.cz>
2019-11-10 11:07:53 +0100

    menu

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Type;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,6 +17,15 @@ class MenuController extends AbstractController {
      * @Route("/menu", methods={"GET", "POST"})
      */
     public function index(){
-        return $this->render('pages/menu/menu.html.twig', []);
+        // naplnění struktury pro výpis tabulky
+        $types = $this->getDoctrine()->getRepository(Type::class)->findAll();
+        $foods = null;
+        $type = null;
+        foreach ($types as $type) {
+            $foods_of_type = $type->getFoods();
+            $foods[$type->getName()] = $foods_of_type;
+        }
+
+        return $this->render('pages/menu/menu.html.twig', array('foods' => $foods));
     }
 }
