d0d078b615c8a2bae969be3f23c5a5026f50268d
TomikRys <tomikrys@seznam.cz>
2019-12-10 07:20:02 +0100

    php word, settings

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -4,10 +4,13 @@
 namespace App\Controller;
 
 
+use App\Entity\Settings;
 use App\Entity\Type;
+use PhpOffice\PhpWord\TemplateProcessor;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Extra\CssInliner\CssInlinerExtension;
 
 /**
  * Class MenuController
@@ -26,16 +29,56 @@ class MenuController extends AbstractController {
             $foods_of_type = $type->getFoods();
             $foods[$type->getName()] = $foods_of_type;
         }
+        $settings = $this->getDoctrine()->getRepository(Settings::class)->find(1);
 
-        return $this->render('pages/menu/menu.html.twig', array('foods' => $foods));
+        return $this->render('pages/menu/menu.html.twig', array('foods' => $foods, 'settings' => $settings));
     }
 
+    /**
+     * @Route("/menu/try", methods={"GET", "POST"})
+     */
+    public function process_word(){
+        $template = new TemplateProcessor('words/template.docx');
+//        $template->setValue('variableName', 'Tomi je pán');
+        $days = ["Pondělí", "Úterý", "Středa"];
+        $mealsTypes = ["Polévka", "Hlavní chod"];
+        $meals = ["pivo", "jidlo"];
+        $price = ["35", "60"];
+        // zkopírování dní dle pole $days
+        $template->cloneBlock('block_days', count($days), true, true);
+        for($i = 0; $i < count($days); $i++) {
+            // zkopírování typů jídel dle pole $mealsTypes
+            $template->cloneBlock('block_mealType#'.($i+1), count($mealsTypes), true, true);
+            // vložení názvů dní
+            $template->setValue('day#'.($i+1), htmlspecialchars($days[$i],ENT_COMPAT, 'UTF-8'));
+            for($j = 0; $j < count($mealsTypes); $j++) {
+                // zkopírování jídel dle pole $meals
+                $template->cloneBlock('block_meals#'.($i+1).'#'.($j+1), count($meals), true, true);
+                // vložení typů jídel
+                $template->setValue('mealType#'.($i+1).'#'.($j+1), htmlspecialchars($mealsTypes[$j],ENT_COMPAT, 'UTF-8'));
+                for($k = 0; $k < count($meals); $k++) {
+                    // vložení jídel
+                    $template->setValue('meal#'.($i+1).'#'.($j+1).'#'.($k+1), htmlspecialchars($meals[$k],ENT_COMPAT, 'UTF-8'));
+                    // vložení cen
+                    $template->setValue('price#'.($i+1).'#'.($j+1).'#'.($k+1), htmlspecialchars($price[$k],ENT_COMPAT, 'UTF-8'));
+                }
+            }
+        }
+
+
+        $template->saveAs('words/result.docx');
+
+        $generated_menu = new File('words/result.docx');
+        return $this->file($generated_menu);;
+    }
 
     /**
      * @Route("/menu/generate", methods={"GET", "POST"})
      */
     public function generate(){
         $menu = json_decode ($_GET["json"]);
+        dump($menu);
+        exit;
         $doGenerate = $_GET["generate"];
 //        dump($menu);
 //        exit;
---
+++ src/Controller/SettingsController.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Settings;
+use App\Entity\Type;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class MenuController
+ * @package App\Controller
+ */
+class SettingsController extends AbstractController {
+
+    public function default_settings() {
+        $settings = new Settings();
+        $days = ["Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek"];
+        $meals = ["Polévka", "Masové", "Vegetariánské"];
+        $settings->setDays($days);
+        $settings->setMeals($meals);
+        $this->getDoctrine()->getManager()->persist($settings);
+        $this->getDoctrine()->getManager()->flush();
+
+        $types = ["polévka", "jídlo"];
+        foreach ($types as $type) {
+            $new_type = new Type();
+            $new_type->setName($type);
+            $this->getDoctrine()->getManager()->persist($new_type);
+            $this->getDoctrine()->getManager()->flush();
+        }
+
+        $tags = ["vege", "spicy", "beef", "lamb", "shrink"];
+        foreach ($tags as $tag) {
+            $new_tag = new Type();
+            $new_tag->setName($tag);
+            $this->getDoctrine()->getManager()->persist($new_tag);
+            $this->getDoctrine()->getManager()->flush();
+        }
+    }
+
+    /**
+     * @Route("/settings", methods={"GET", "POST"})
+     */
+    public function index(){
+        $this->default_settings();
+        // naplnění struktury pro výpis tabulky
+        $settings = $this->getDoctrine()->getRepository(Settings::class)->find(1);
+        $types = $this->getDoctrine()->getRepository(Type::class)->findAll();
+
+        return $this->render('pages/settings/settings.html.twig', array('settings' => $settings, 'types' => $types));
+    }
+
+}
