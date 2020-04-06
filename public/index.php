4096fb0cb0e07b2322a6442cd0cb1d569a7be834
Tomáš Ryšavý <xrysav27@vutbr.cz>
2020-04-06 16:06:49 +0200

    https localhost fix

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-if($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+if(isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
 {
     $_SERVER['HTTPS'] = 'on';
     $_SERVER['SERVER_PORT'] = 443;
